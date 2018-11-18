c8a16def38eac467287188f9953345a3c6bb5e95
noobow34 <skywind.hk34@gmail.com>
2018-11-19 00:46:50 +0900

    analytics実装中

--- NoobowNotifier/LineBotApp.cs
+++ NoobowNotifier/LineBotApp.cs
@@ -1,6 +1,12 @@
+using Google.Apis.Analytics.v3;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
 using Line.Messaging;
 using Line.Messaging.Webhooks;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace NoobowNotifier
@@ -28,8 +34,29 @@ namespace NoobowNotifier
         {
             userMessage = userMessage.ToLower().Replace(" ", "");
             ISendMessage replyMessage = null;
+            var certificate = new X509Certificate2(@"../keys/noobow.p12", "notasecret", X509KeyStorageFlags.Exportable);
+            var credential = new ServiceAccountCredential(new ServiceAccountCredential.Initializer("noobow@noobow-222722.iam.gserviceaccount.com")
+            {
+                Scopes = new[] { AnalyticsService.Scope.Analytics, AnalyticsService.Scope.AnalyticsReadonly }
+            }.FromCertificate(certificate));
+            var service = new AnalyticsService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "NoobowNotifier",
+            });
 
-            replyMessage = new TextMessage($"You said: {userMessage}");
+            var todayString = DateTime.Now.ToString("yyyy-MM-dd");
+            var yesterdayString = DateTime.Now.AddDays(-1).ToString("yyyy-MM-dd");
+
+            var userData = await service.Data.Ga.Get("ga:181171503", todayString, todayString,  "ga:users").ExecuteAsync();
+
+            string userCount = "0";
+            if(userData.Rows != null)
+            {
+                userCount = userData.Rows[0][0];
+            }
+
+            replyMessage = new TextMessage($"ç°ì˙: {userCount}");
 
             await messagingClient.ReplyMessageAsync(replyToken, new List<ISendMessage> { replyMessage });
         }
