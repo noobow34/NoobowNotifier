a59943480b00e19de649baaf3d0e34eb421fdd29
noobow34 <skywind.hk34@gmail.com>
2018-11-25 14:22:08 +0900

    ログ件数追加

--- NoobowNotifier/Logics/GALogics.cs
+++ NoobowNotifier/Logics/GALogics.cs
@@ -1,4 +1,6 @@
-﻿using NoobowNotifier.Constants;
+﻿using jafleet.Constants;
+using jafleet.EF;
+using NoobowNotifier.Constants;
 using NoobowNotifier.Services;
 using System;
 using System.Collections.Generic;
@@ -58,8 +60,27 @@ namespace NoobowNotifier.Logics
                     }
                 }
             }
+
+            string todaySqlite = DateTime.Now.ToString("yyyy-MM-dd");
+            string yesterdaySqlite = DateTime.Now.AddDays(-1).ToString("yyyy-MM-dd");
+            int todayPhoto = 0, todayLine = 0, todaySearch = 0, todayEx = 0;
+            int yesterdayPhoto = 0, yesterdayLine = 0, yesterdaySearch = 0, yesterdayEx = 0;
+
+            using (var context = new jafleetContext())
+            {
+                todayPhoto = context.Log.Where(q => q.LogDate.StartsWith(todaySqlite)).Where(q => q.LogType == LogType.PHOTO).Where(q => q.UserId != "True").Where(q => q.UserId != "U68e05e69b6acbaaf565bc616fdef695d").OrderByDescending(q => q.LogDate).Count();
+                todayLine = context.Log.Where(q => q.LogDate.StartsWith(todaySqlite)).Where(q => q.LogType == LogType.LINE).Where(q => q.UserId != "True").Where(q => q.UserId != "U68e05e69b6acbaaf565bc616fdef695d").OrderByDescending(q => q.LogDate).Count();
+                todaySearch = context.Log.Where(q => q.LogDate.StartsWith(todaySqlite)).Where(q => q.LogType == LogType.SEARCH).Where(q => q.UserId != "True").Where(q => q.UserId != "U68e05e69b6acbaaf565bc616fdef695d").OrderByDescending(q => q.LogDate).Count();
+                todayEx = context.Log.Where(q => q.LogDate.StartsWith(todaySqlite)).Where(q => q.LogType == LogType.EXCEPTION).Where(q => q.UserId != "True").Where(q => q.UserId != "U68e05e69b6acbaaf565bc616fdef695d").OrderByDescending(q => q.LogDate).Count();
+                yesterdayPhoto = context.Log.Where(q => q.LogDate.StartsWith(yesterdaySqlite)).Where(q => q.LogType == LogType.PHOTO).Where(q => q.UserId != "True").Where(q => q.UserId != "U68e05e69b6acbaaf565bc616fdef695d").OrderByDescending(q => q.LogDate).Count();
+                yesterdayLine = context.Log.Where(q => q.LogDate.StartsWith(yesterdaySqlite)).Where(q => q.LogType == LogType.LINE).Where(q => q.UserId != "True").Where(q => q.UserId != "U68e05e69b6acbaaf565bc616fdef695d").OrderByDescending(q => q.LogDate).Count();
+                yesterdaySearch = context.Log.Where(q => q.LogDate.StartsWith(yesterdaySqlite)).Where(q => q.LogType == LogType.SEARCH).Where(q => q.UserId != "True").Where(q => q.UserId != "U68e05e69b6acbaaf565bc616fdef695d").OrderByDescending(q => q.LogDate).Count();
+                yesterdayEx = context.Log.Where(q => q.LogDate.StartsWith(yesterdaySqlite)).Where(q => q.LogType == LogType.EXCEPTION).Where(q => q.UserId != "True").Where(q => q.UserId != "U68e05e69b6acbaaf565bc616fdef695d").OrderByDescending(q => q.LogDate).Count();
+            }
             return $"今日：u{todayUsers},s{todaySessions},pv{todayPageViews}\n" +
-                $"昨日：u{yesterdayUsers},s{yesterdaySessions},pv{yesterdayPageViews}";
+                $"photo{todayPhoto},line{todayLine},search{todaySearch},ex{todayEx}\n" +
+                $"昨日：u{yesterdayUsers},s{yesterdaySessions},pv{yesterdayPageViews}\n" +
+                $"photo{yesterdayPhoto},line{yesterdayLine},search{yesterdaySearch},ex{yesterdayEx}";
         }
     }
 }
