810906f0008ac6bcaae8190e1db12ff449b289c0
noobow34 <skywind.hk34@gmail.com>
2019-02-10 21:47:21 +0900

    自分にPushするエンドポイントを設置 #7

---
+++ NoobowNotifier/Constants/UserIdConstant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoobowNotifier.Constants
+{
+    public static class UserIdConstant
+    {
+        public const string NOOBWO_USER_ID = "U68e05e69b6acbaaf565bc616fdef695d";
+    }
+}
--- NoobowNotifier/Controllers/LineBotController.cs
+++ NoobowNotifier/Controllers/LineBotController.cs
@@ -4,6 +4,8 @@ using Newtonsoft.Json.Linq;
 using System.Threading.Tasks;
 using NoobowNotifier.Manager;
 using jafleet.Commons.EF;
+using Line.Messaging;
+using System.Collections.Generic;
 
 namespace NoobowNotifier.Controllers
 {
@@ -17,6 +19,8 @@ namespace NoobowNotifier.Controllers
             _context = context;
         }
 
+        public string NOOBWO_USER_ID { get; private set; }
+
         /// <summary>
         /// POST: api/Messages
         /// Receive a message from a user and reply to it
@@ -28,6 +32,18 @@ namespace NoobowNotifier.Controllers
 
             var app = new LineBotApp(LineMessagingClientManager.GetInstance(),_context);
             await app.RunAsync(events);
+            return new OkResult();
+        }
+
+        /// <summary>
+        /// POST: api/NotifyMe
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]string message)
+        {
+            await LineMessagingClientManager.GetInstance().PushMessageAsync(NOOBWO_USER_ID, new List<ISendMessage>() { new TextMessage(message) });
+
             return new OkResult();
         }
     }
