efbc1bcd875a59e4bbe150b90cb1f5609d6f0840
noobow34 <skywind.hk34@gmail.com>
2019-11-17 14:49:28 +0900

    シェル実行テスト

---
+++ NoobowNotifier/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NoobowNotifier.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            string command = @"/home/noobow/test.sh";
+
+
+            ProcessStartInfo psInfo = new ProcessStartInfo();
+
+            psInfo.FileName = command;
+            psInfo.CreateNoWindow = true;
+            psInfo.UseShellExecute = false;
+
+            psInfo.RedirectStandardOutput = true;
+
+            Process p = Process.Start(psInfo);
+            string output = p.StandardOutput.ReadToEnd();
+
+            return Content(output);
+        }
+    }
+}
